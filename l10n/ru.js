fcb5da8f7f7bbc2340cbe79ce1b47f747fe775e1
GrawlCore <Grawl@ya.ru>
2011-11-20 04:09:29 +1100

    initial

---
+++ l10n/ru.js
@@ -0,0 +1,4 @@
+var strings = {
+	'Flash is not installed on this browser.  Hurray!' : 'Flash Player не установлен в этом браузере! Ура!',
+	'Flash is currently installed on this browser' : 'Flash установлен в этом браузере'
+};
